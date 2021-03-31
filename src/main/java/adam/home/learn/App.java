eb1fd7c546f039237edd80dc00cbcf72b0ffdf75
mr-jackpot <adam.heeps@live.co.uk>
2021-03-31 21:02:12 +0100

    Worked on my SQL connection manager. Now removed hardcoded db details (security).

--- src/main/java/adam/home/learn/App.java
+++ src/main/java/adam/home/learn/App.java
@@ -1,16 +1,18 @@
 package adam.home.learn;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Attempting to connect to SQL Server..." );
-        
-        SQLDatabaseConnection conn = new SQLDatabaseConnection();
-        conn.establishConnection();
-    }
+import java.sql.Connection;
+import java.util.Scanner;
+
+public class App {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Please enter the database, username, & password: ");
+		Scanner input = new Scanner(System.in);
+		SQLDatabase sandbox = new SQLDatabase(input.nextLine(), input.nextLine(), input.nextLine());
+		input.close();
+		Connection conn = sandbox.establishConnection();
+		System.out.println("INFO - Active SQL Server Connection: " + sandbox.connectionStatus(conn));
+
+	}
 }
