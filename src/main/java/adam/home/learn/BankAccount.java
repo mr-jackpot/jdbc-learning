8d578602cbe126e989a7a47bca55023cd22602e8
mr-jackpot <adam.heeps@live.co.uk>
2021-04-10 10:08:09 +0100

    Created Bank Account class and added methods.

---
+++ src/main/java/adam/home/learn/BankAccount.java
@@ -0,0 +1,41 @@
+package adam.home.learn;
+
+public class BankAccount {
+
+	final String BANK = "BANK OF SCOTLAND";
+	String fullName;
+	String country; 
+	String city;
+	double balance;
+	
+	public BankAccount(String name, String location, String town, double openingBalance) {
+
+		fullName = name;
+		country = location;
+		city = town;
+		balance = openingBalance;
+	}
+	
+	
+	public void depositMoney() {
+		
+	}
+	
+	public void withdrawMoney() {
+		
+	}
+	
+	public void changeName() {
+		
+	}
+	
+	public void changeAddress() {
+		
+	}
+	
+	public void closeAccount() {
+		
+	}
+	
+	
+}
